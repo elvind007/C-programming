db1f6a93fdcbd3fffbb55502b76d1406e1b0dc65
Elvin Dsouza <emdd007@gmail.com>
2023-02-25 08:12:36 +0530

    done

---
+++ 2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int a = 9,b = 4, c;
+    
+    c = a+b;
+    printf("a+b = %d \n",c);
+    c = a-b;
+    printf("a-b = %d \n",c);
+    c = a*b;
+    printf("a*b = %d \n",c);
+    c = a/b;
+    printf("a/b = %d \n",c);
+    c = a%b;
+    printf("Remainder when a divided by b = %d \n",c);
+    
+    return 0;
+}
---
+++ scan.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int num;
+    printf("Enter the Number: ");
+    scanf("%d", &num);
+    printf("\nYou've entered: %d", num);
+    getch();
+    return 0;
+}
